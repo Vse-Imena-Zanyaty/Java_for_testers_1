832c1a4e3a5a9496516ed6aab0a2bd1ec31f4b6f
Vse-Imena-Zanyaty <47252441+Vse-Imena-Zanyaty@users.noreply.github.com>
2019-02-03 13:31:56 +0300

    Code reformatting
    Лекция 1.2. Фигурные скобочки и структура программного кода

--- sandbox/src/main/java/Lecture11/sandbox/Lecture03.java
+++ sandbox/src/main/java/Lecture11/sandbox/Lecture03.java
@@ -2,8 +2,8 @@ package Lecture11.sandbox;
 
 public class Lecture03 {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
 }
