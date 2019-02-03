02b775313995e044f409b36b5ec5a6ef475bf83b
Vse-Imena-Zanyaty <47252441+Vse-Imena-Zanyaty@users.noreply.github.com>
2019-02-03 15:02:59 +0300

    Added several functions
    Лекция 1.7. Функции

--- sandbox/src/main/java/Lecture11/sandbox/Lecture03.java
+++ sandbox/src/main/java/Lecture11/sandbox/Lecture03.java
@@ -3,7 +3,28 @@ package Lecture11.sandbox;
 public class Lecture03 {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world!");
+    hello("Olga!");
+    hello("user!");
+
+    double c = 5;
+    System.out.println("Area of square with sides " + c + " = " + area(c));
+
+    double d = 4;
+    double e = 6;
+    System.out.println("Area of rectangle with sides " + d + " and " + e + " = " + area(d, e));
+  }
+
+  public static void hello(String a) {
+    System.out.println("Hello, " + a);
+  }
+
+  public static double area(double b) {
+    return b * b;
+  }
+
+  public static double area(double d, double e) {
+    return d * e;
   }
 
 }
