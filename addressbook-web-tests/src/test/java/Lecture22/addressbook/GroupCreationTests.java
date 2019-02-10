c4a1a952936f2bfbb3bae64a8ccbbfe979cf4751
Vse-Imena-Zanyaty <47252441+Vse-Imena-Zanyaty@users.noreply.github.com>
2019-02-10 21:05:23 +0300

    Refactoring
    Лекция 2.4. Рефакторинг: улучшаем читаемость кода, выделяем вспомогательные методы

--- addressbook-web-tests/src/test/java/Lecture22/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/Lecture22/addressbook/GroupCreationTests.java
@@ -10,35 +10,56 @@ public class GroupCreationTests {
   private ChromeDriver wd;
 
   @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
+  public void setUp() throws Exception {
     System.setProperty("webdriver.chrome.wd", "C:/Windows/System32/chromedriver.exe");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    wd.findElement(By.linkText("groups")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=groups | ]]
-    wd.findElement(By.name("new")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=new | ]]
-    wd.findElement(By.name("group_name")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_name | ]]
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("group1");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("header1");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("comment1");
-    wd.findElement(By.name("submit")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new Group("group1", "header1", "comment1"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
     wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(Group group) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(group.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(group.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(group.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
