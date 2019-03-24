c14b836ff0035ec30195eff5eebe387aa9d75520
Vse-Imena-Zanyaty <47252441+Vse-Imena-Zanyaty@users.noreply.github.com>
2019-03-24 19:50:57 +0300

    Hibernate for ORM
    Лекция 7.2. Object Relational Mapping (ORM)

---
+++ addressbook-web-tests/src/test/java/Lecture22/addressbook/tests/HBconnection.java
@@ -0,0 +1,45 @@
+package Lecture22.addressbook.tests;
+
+import Lecture22.addressbook.objects.Group;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HBconnection {
+
+  private SessionFactory sessionFactory;
+
+  @BeforeClass
+  protected void setUp() throws Exception {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure() // configures settings from hibernate.cfg.xml
+            .build();
+    try {
+      sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    } catch (Exception e) {
+      e.printStackTrace();
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy(registry);
+    }
+  }
+
+  @Test
+  public void testHibernateConnection() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<Group> result = session.createQuery("from Group").list();
+    for (Group group : result) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+}
